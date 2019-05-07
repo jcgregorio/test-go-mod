80b6eec8b901749aeb784a07f3a1feeac04ed0ac
Joe Gregorio <jcgregorio@google.com>
2019-05-07 09:46:51 -0400

    Now remove a dep from the code, but don't run tidy.

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/shenwei356/bwt"
 )
 
 func main() {
 	fmt.Println("vim-go")
-	s := "abracadabra"
-	tr, _, err := bwt.Transform([]byte(s), '$')
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(tr))
 }
